19b63c3c59adf9666b88c8242c8682d5e8d8306e
Simona Brabcová <simona.brabcova@email.cz>
2022-02-15 23:14:28 +0100

    přidání tlačítek k odstavci a skladbě

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 // tady je místo pro náš program
+let paragraph = document.querySelector('p');
+paragraph.style.fontSize = '16px';
+
+function priKliknuti() {
+    paragraph.classList.toggle('red');
+}
+
+function priKliknuti2(){
+    paragraph.style.fontSize = '17px';
+}
+
+let song = document.querySelector('#song');
+function play(){
+    song.play();
+}
+
+function pause(){
+    song.pause();
+}
+
+function volumeDown(){
+    song.volume = 0;
+}
+
+function volumeNormal(){
+    song.volume = 0.5;
+}
+
+function volumeUp(){
+    song.volume = 1;
+}
+
+function backToStart(){
+    song.currentTime = 0;
+}
